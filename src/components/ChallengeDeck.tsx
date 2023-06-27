c213daad85dc2207e51e1a54a5ab0f69184e6d46
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2023-06-27 09:11:16 +0200

    Fix function type

--- src/components/ChallengeDeck.tsx
+++ src/components/ChallengeDeck.tsx
@@ -19,7 +19,7 @@ interface IChallengeProps {
   skipScore: number;
   arrowText: string;
   flipped: boolean;
-  onClick(e: React.MouseEvent, animationDuration: number): unknown;
+  onClick(e: React.MouseEvent): unknown;
 }
 
 // If loading a variable font, you don't need to specify the font weight
--- src/components/ThrowDeck.tsx
+++ src/components/ThrowDeck.tsx
@@ -16,7 +16,7 @@ interface IThrowProps {
   winScore: number;
   arrowText: string;
   flipped: boolean;
-  onClick(e: React.MouseEvent, animationDuration: number): unknown;
+  onClick(e: React.MouseEvent): unknown;
 }
 
 // If loading a variable font, you don't need to specify the font weight
