1089469670e9f5b9e42396280f17eed4b27ce153
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2024-01-27 23:11:58 +0100

    Mahe PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === "development",
+})
+
+const nextConfig = withPWA({
   experimental: {
     appDir: true,
   },
-}
+  reactStrictMode: false,
+  swcMinify: false,
+})
 
 module.exports = nextConfig
